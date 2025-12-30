46843d0cd7d7202ee1c9b5ae55da4d4dae296686
luxipha <abisoye.bello@gmail.com>
2025-12-30 13:27:47 +0700

    fixing asset issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
 import { CasperProvider } from '@/lib/casper-context';
+import AssetPreloader from '@/components/asset-preloader';
 
 const font = Outfit({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <CasperProvider>{children}</CasperProvider>
+        <AssetPreloader />
       </body>
     </html>
   );
